0d273e65de55e7ce98148c525a1031dbcc459033
David Bonet <webbonet@gmail.com>
2017-09-29 07:57:21 +0200

    Add missing files

---
+++ src/transliterate.rs
@@ -0,0 +1,91 @@
+use locale_ffi::{uselocale, newlocale, __locale_struct};
+use std::ptr;
+use std::ffi::CString;
+use libc::{LC_ALL_MASK, free, c_void};
+use iconv::{Iconv};
+
+#[derive(Debug)]
+pub struct TextTransliterate;
+
+impl TextTransliterate {
+	pub fn new() -> TextTransliterate {
+		TextTransliterate {}
+	}
+
+	fn set_thread_locale<S: Into<String>>(&self, locale: S) -> Result<*mut __locale_struct, &'static str> {
+
+		
+		if let Ok(locale) = CString::new(locale.into()) {
+
+			let locale = locale.as_ptr();
+
+			let null: *mut __locale_struct = ptr::null_mut();
+			
+			let old_locale = unsafe { 
+				let locale = newlocale(LC_ALL_MASK, locale, null);
+				uselocale(locale)
+			};
+
+			Ok(old_locale)
+
+		} else {
+			Err("Not able to decode locale text")
+		}
+	}
+
+	fn restore_thread_locale(&self, old_locale: *mut __locale_struct) {
+		unsafe { 
+			let old_locale = uselocale(old_locale);
+			free(old_locale as *mut c_void)
+		};
+	}
+
+	pub fn transliterate<S: Into<String>, T: Into<String>>(&self, text: S, locale: T) -> Result<String, &'static str> {
+		let old_locale = self.set_thread_locale(locale);
+
+		if let Ok(old_locale) = old_locale {
+			
+			let iconv = Iconv::new("ascii//TRANSLIT//IGNORE", "utf-8");
+
+			if let Ok(mut iconv) = iconv {
+				
+				let mut buf = Vec::new();
+				let result = iconv.convert(&text.into().as_bytes(), &mut buf, 0);
+
+				if let Err(_) = result {
+					return Err("Error in transliteration");
+				}
+				let output_utf8 = String::from_utf8(buf);
+
+				self.restore_thread_locale(old_locale);
+
+				if let Ok(output) = output_utf8 {
+					Ok(output)
+				} else {
+					Err("Error in the transliteration")
+				}
+			} else {
+				Err("Not possible initialize iconv")
+			}
+		} else {
+			Err("Not possible to set the locale")
+		}
+	}
+}
+
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn it_works() {
+		let tt = TextTransliterate::new();
+		let result = tt.transliterate("ü  ä  ö  ß  Ü  Ä  Ö ç ñ 的 😒", "de_DE.UTF-8");
+		if let Ok(result) = result {
+			assert_eq!("ue  ae  oe  ss  UE  AE  OE c n ? ?", result);
+		} else {
+			assert!(false);
+		}
+	}
+}
---
+++ src/transliterate_async.rs
@@ -0,0 +1,82 @@
+use transliterate::TextTransliterate;
+use std::sync::mpsc::{Sender, Receiver};
+use std::sync::mpsc;
+use std::thread;
+
+#[derive(Debug)]
+enum TransliterateRequestAction {
+	Die,
+	Transliterate
+}
+
+#[derive(Debug)]
+struct TransliterationRequest {
+	text: String,
+	sender: Sender<Result<String, &'static str>>,
+	locale: String,
+	action: TransliterateRequestAction
+}
+
+#[derive(Debug)]
+struct TransliterateAsync {
+	sender: Sender<TransliterationRequest>
+}
+
+impl TransliterateAsync {
+	pub fn new() -> TransliterateAsync {
+		let (sender, receiver): (Sender<TransliterationRequest>, Receiver<TransliterationRequest>) = mpsc::channel();
+
+		let text_transliterate = TextTransliterate::new();
+
+		TransliterateAsync::create_thread(receiver, text_transliterate);
+
+		TransliterateAsync {sender: sender}
+	}
+
+	pub fn transliterate<S: Into<String>, T: Into<String>>(&self, text: S, locale: T) -> Result<String, &'static str> {
+		let (sender, receiver): (Sender<Result<String, &'static str>>, Receiver<Result<String, &'static str>>) = mpsc::channel();
+
+		self.sender.send(TransliterationRequest {
+			text: text.into(),
+			sender: sender,
+			locale: locale.into(),
+			action: TransliterateRequestAction::Transliterate
+		});
+
+		if let Ok(result) = receiver.recv() {
+			match result {
+				Ok(text) => Ok(text),
+				Err(message) => Err(message),
+			}
+		} else {
+			Err("Error communicating with the thread")
+		}
+
+	}
+
+	fn create_thread(receiver: Receiver<TransliterationRequest>, tt: TextTransliterate) {
+		thread::spawn(move || {
+			while let Ok(request) = receiver.recv() {
+				if let Ok(result) = tt.transliterate(request.text, request.locale){
+					request.sender.send(Ok(result)).unwrap();
+				}
+			}
+		});
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn it_works() {
+		let tt = TransliterateAsync::new();
+		let result = tt.transliterate("ü  ä  ö  ß  Ü  Ä  Ö ç ñ 的 😒", "de_DE.UTF-8");
+		if let Ok(result) = result {
+			assert_eq!("ue  ae  oe  ss  UE  AE  OE c n ? ?", result);
+		} else {
+			assert!(false);
+		}
+	}
+}
