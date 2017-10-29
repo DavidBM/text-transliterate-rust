fc655bd04fa10be67f45a4b2272b62590a563b74
David Bonet Montes <webbonet@gmail.com>
2017-10-29 19:04:18 +0100

    add iconv wrapper

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
 extern crate libc;
-extern crate iconv;
+extern crate errno;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
 
+mod iconv;
 mod locale_ffi;
 mod transliterate;
 mod transliterate_async;
 
 pub use transliterate::TextTransliterate;
 pub use transliterate_async::TextTransliterateAsync;
+pub use iconv::Iconv;
+
